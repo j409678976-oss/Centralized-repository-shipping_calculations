5bdc8a5d392574e2ff5869b76480db353c47b0d7
jeneen-jjj <j409678976@gmail.com>
2025-12-02 14:57:06 +0200

    Update Shipping_Cost_Calculator.py

--- Shipping_Cost_Calculator.py
+++ Shipping_Cost_Calculator.py
@@ -1,3 +1,4 @@
+ # Here is a new update by <j409678976-oss>
 # Shipping Cost Calculator
 
 ## Input package weight and shipping rate
@@ -9,5 +10,3 @@ shipping_cost = weight * rate
 
 ## Display the result
 print(f"Shipping Cost: {shipping_cost} USD")
-
- # Here is a new update by <j409678976-oss>
